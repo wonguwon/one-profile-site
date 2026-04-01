b7fcc60a8aa8a8456e068c7241fa2f8c49750c92
wonguwon25 <wonguwon25@gmail.com>
2026-04-01 22:04:16 +0900

    add site domain

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://edu.oneground.ai.kr',
   integrations: [tailwind()],
   output: 'static',
 });
